baab3bc3aa03802a6cd87082a9a570fcbcb0ce2f
wpdudH <wpdudghd@gmail.com>
2025-01-01 19:57:31 +0900

    [ADD] solve n8393

---
+++ InputOutput/n8393.cpp
@@ -0,0 +1,28 @@
+/*
+문제
+n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다
+
+출력
+1부터 n까지 합을 출력한다.
+*/
+
+#include <iostream>
+
+int main()
+{
+	int num;
+	int answer = 0;
+
+	std::cin >> num;
+
+	for (int i = 1; i <= num; i++)
+	{
+		answer += i;
+	}
+	std::cout << answer;
+
+	return 0;
+}
