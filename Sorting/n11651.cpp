0e83f65ab1627a1d377fd29a22af352369e4b7fb
wpdudH <66068922+wpdudH@users.noreply.github.com>
2025-03-09 22:30:49 +0900

    [ADD] solve n11651

---
+++ Sorting/n11651.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+struct coord {
+	int x;
+	int y;
+};
+
+bool compare(coord a, coord b) {
+	if (a.y == b.y) {			
+		return a.x < b.x;		
+	}
+	else {						
+		return a.y < b.y;		
+	}
+}
+
+int main(void) {
+
+	std::cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	int n, tmp;
+	std::cin >> n;
+
+	coord arr[100001];
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> arr[i].x >> arr[i].y;
+	}
+
+	std::sort(arr, arr + n, compare);
+
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i].x << " " << arr[i].y << "\n";
+	}
+
+
+	return 0;
+}
