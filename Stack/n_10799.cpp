5c95bed71447db5e2a41933c160d75bb331ad8c9
wpdudH <66068922+wpdudH@users.noreply.github.com>
2025-03-15 17:41:13 +0900

    [ADD] solve n10799

---
+++ Stack/n_10799.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+#include<stack>
+#include<string.h>
+using namespace std;
+int main()
+{
+	stack<int> s;
+	string str;
+
+	int cnt = 0;
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == '(' && str[i + 1] == ')')
+		{
+			cnt += s.size();
+			i++;
+		}
+		else if (str[i] == '(')
+		{
+			s.push(i);
+		}
+		else if (str[i] == ')')
+		{
+			cnt++;
+			s.pop();
+		}
+	}
+	cout << cnt;
+}
